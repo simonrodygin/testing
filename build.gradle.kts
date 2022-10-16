c5f1b035a56bd75ee6ba485ca052f70500012d71
simon <simonrodygin@gmail.com>
2022-10-16 15:01:29 +0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(group = "org.seleniumhq.selenium", name = "selenium-java", version = "4.5.0")
+    implementation("io.github.bonigarcia:webdrivermanager:5.3.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+
---
+++ src/test/TestforTest.kt
@@ -0,0 +1,9 @@
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class TestAmazonCart {
+    @Test
+    fun `everything is fine`() {
+        assertTrue(true)
+    }
+}
